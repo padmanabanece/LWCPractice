93b5c909e381062d0f7f611273ba992e0c027ae6
padmanaban <softtest.spring@gmail.com>
2021-12-29 22:36:50 +0530

    final commit on calculator example

--- Lwc Practice/force-app/main/default/lwc/calculatorExample/calculatorExample.js
+++ Lwc Practice/force-app/main/default/lwc/calculatorExample/calculatorExample.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class CalculatorExample extends LightningElement {}
+export default class CalculatorExample extends LightningElement {
+    firstNumber;
+    lastNumber;
+    result;
+    handleChange(event){
+        if (event.target.name=='FNumber') {
+            this.firstNumber= event.target.value;
+        }
+        else if(event.target.name=='LNumber'){
+            this.lastNumber=event.target.value;
+        }
+    }
+    
+    handleAdded(){
+       this.result=parseInt(this.firstNumber) + parseInt(this.lastNumber);
+    }
+}
